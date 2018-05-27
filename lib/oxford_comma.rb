570d490a25e0eaabb4b53bc938888f120b1bbc94
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 18:41:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  converted_string = ""
+  array.each_index do |i|
+    if array.length == 1
+      converted_string << "#{array[i]}"
+    elsif array.length == 2 
+      if i == array.length - 1
+        converted_string << "and #{array[i]}"
+      else 
+        converted_string << "#{array[i]} "
+      end  
+    elsif array.length > 1 && i == array.length - 1 
+      converted_string << "and #{array[i]}"
+    else  
+    converted_string << "#{array[i]}, "
+    end
+  end
+    converted_string
 end
+  
