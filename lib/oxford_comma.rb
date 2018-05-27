edd88ba70e5000a27cf84331cde5d67d96743075
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 18:13:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
   converted_string = ""
   array.each_index |i|
-  
+    if i == array.length 
+      converted_string << "and #{array[i]}"
+    else  
+    converted_string << "#{array[i], "
+  end
+  converted_string
 end
