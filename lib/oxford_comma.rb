9df028c3d1e0405f93ea6122d5d777c4f45297d6
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 18:37:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     if array.length == 1
       converted_string << "#{array[i]}"
     elsif array.length == 2 
-      if array[1]
+      if array.length - 1
         converted_string << "and #{array[i]}"
       else 
         converted_string << "#{array[i]} "
